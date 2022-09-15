2817e030c73379b14abe9a87f677dca4e37295a0
Juan Casa <juanestebanlolx@gmail.com>
2022-09-14 23:11:28 -0500

    Curso de React.js

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React  from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return (
+        <h1>Hola Mundo, pglo con la liberia de React mi bro!</h1>
+    );
+}
+
+export default App;
